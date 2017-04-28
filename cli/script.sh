b32a153d18577ea420e6ffe1cbe4e23c9bcbd2bf
Angel Milanov <milanov@de.ibm.com>
2017-04-28 09:29:43 +0200

    Adds automatic BC bootstrapping.

--- cli/script.sh
+++ cli/script.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e
 
-CHANNEL_NAME="default"
+CORE_PEER_MSPCONFIGPATH=/peers/orderer/localMspConfig CORE_PEER_LOCALMSPID="OrdererMSP" peer channel create -o orderer0:7050 -c default -f /peers/orderer/channel.tx --tls true --cafile /peers/orderer/localMspConfig/cacerts/ordererOrg.pem
+
+CORE_PEER_MSPCONFIGPATH=/peers/insurancePeer/localMspConfig CORE_PEER_ADDRESS=insurance-peer:7051 CORE_PEER_LOCALMSPID=InsuranceOrgMSP CORE_PEER_TLS_ROOTCERT_FILE=/peers/insurancePeer/localMspConfig/cacerts/insuranceOrg.pem peer channel join -b default.block
+
+CORE_PEER_MSPCONFIGPATH=/peers/shopPeer/localMspConfig CORE_PEER_ADDRESS=shop-peer:7051 CORE_PEER_LOCALMSPID=ShopOrgMSP CORE_PEER_TLS_ROOTCERT_FILE=/peers/shopPeer/localMspConfig/cacerts/shopOrg.pem peer channel join -b default.block
+
+CORE_PEER_MSPCONFIGPATH=/peers/repairServicePeer/localMspConfig CORE_PEER_ADDRESS=repairservice-peer:7051 CORE_PEER_LOCALMSPID=RepairServiceOrgMSP CORE_PEER_TLS_ROOTCERT_FILE=/peers/repairServicePeer/localMspConfig/cacerts/repairServiceOrg.pem peer channel join -b default.block
