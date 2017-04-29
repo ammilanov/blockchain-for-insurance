0180f1171e1cae22f601559baafa30424c9a8bc8
Marvin Wiegand <mwiegand@de.ibm.com>
2017-04-29 18:11:59 +0200

    implements createUser

--- web/chaincode/src/bcins/invoke_shop.go
+++ web/chaincode/src/bcins/invoke_shop.go
@@ -1,14 +1,41 @@
 package main
 
-import "github.com/hyperledger/fabric/core/chaincode/shim"
-import pb "github.com/hyperledger/fabric/protos/peer"
+import (
+	"encoding/json"
 
-func createContract(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	//TODO: createContract
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+func createContrau(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	//TODO: createContrau
 	return shim.Success(nil)
 }
 
 func createUser(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	//TODO: createUser
+	var err error
+
+	u := &user{}
+
+	err = json.Unmarshal([]byte(args[0]), u)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	key, err := stub.CreateCompositeKey(prefixUser, []string{u.Username})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	value, err := json.Marshal(u)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	err = stub.PutState(key, value)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
 	return shim.Success(nil)
 }
