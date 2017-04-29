167b368de3a1c28a3be88bc0d770909ae9a4916a
Marvin Wiegand <mwiegand@de.ibm.com>
2017-04-29 19:03:25 +0200

    implements update user in createContract

--- web/chaincode/src/bcins/invoke_shop.go
+++ web/chaincode/src/bcins/invoke_shop.go
@@ -16,28 +16,53 @@ func createContract(stub shim.ChaincodeStubInterface, args []string) pb.Response
 
 	dto := &contractDTO{}
 	c := &contract{}
+	u := &user{}
 
+	// Extract UUID for contract
 	err = json.Unmarshal([]byte(args[0]), dto)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
+	// Extract contract
 	err = json.Unmarshal([]byte(args[0]), c)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
+	// Update User with new Contract
+	readUserAsbytes, err := stub.GetState(c.Username)
+	if err != nil {
+		res := "Failed to get state for " + c.Username
+		return shim.Error(res)
+	} else if readUserAsbytes == nil {
+		res := "User does not exist: " + c.Username
+		return shim.Error(res)
+	}
+	err = json.Unmarshal(readUserAsbytes, u)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	u.ContractIndex = append(u.ContractIndex, dto.UUID)
+	writeUserAsBytes, err := json.Marshal(u)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	err = stub.PutState(u.Username, writeUserAsBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Write contract to State
 	key, err := stub.CreateCompositeKey(prefixContract, []string{dto.UUID})
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-
-	value, err := json.Marshal(c)
+	contractAsBytes, err := json.Marshal(c)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-
-	err = stub.PutState(key, value)
+	err = stub.PutState(key, contractAsBytes)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
