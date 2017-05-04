743181b70baf690e91425b0bce79a0d91af1a6d1
Angel Milanov <milanov@de.ibm.com>
2017-05-04 12:33:37 +0200

    Switch to non-encrypted connection blockchain network.

--- web/www/blockchain/config.js
+++ web/www/blockchain/config.js
@@ -8,15 +8,15 @@ let config = {
   chaincodePath: 'bcins',
   orderer0: {
     hostname: 'orderer0',
-    url: 'grpcs://orderer0:7050',
+    url: 'grpc://orderer0:7050',
     pem: ''
   },
   insuranceOrg: {
     peer: {
       hostname: 'insurance-peer',
-      url: 'grpcs://insurance-peer:7051',
+      url: 'grpc://insurance-peer:7051',
       pem: '',
-      eventHubUrl: 'grpcs://insurance-peer:7053',
+      eventHubUrl: 'grpc://insurance-peer:7053',
     },
     ca: {
       hostname: 'insurance-ca',
@@ -27,9 +27,9 @@ let config = {
   shopOrg: {
     peer: {
       hostname: 'shop-peer',
-      url: 'grpcs://shop-peer:7051',
+      url: 'grpc://shop-peer:7051',
       pem: '',
-      eventHubUrl: 'grpcs://shop-peer:7053',
+      eventHubUrl: 'grpc://shop-peer:7053',
     },
     ca: {
       hostname: 'shop-ca',
@@ -40,9 +40,9 @@ let config = {
   repairServiceOrg: {
     peer: {
       hostname: 'repairserivce-peer',
-      url: 'grpcs://repairservice-peer:7051',
+      url: 'grpc://repairservice-peer:7051',
       pem: '',
-      eventHubUrl: 'grpcs://repairservice-peer:7053',
+      eventHubUrl: 'grpc://repairservice-peer:7053',
     },
     ca: {
       hostname: 'repairservice-ca',
@@ -53,26 +53,26 @@ let config = {
 }
 
 if (process.env.LOCALCONFIG) {
-  config.orderer0.url = 'grpcs://localhost:7050';
+  config.orderer0.url = 'grpc://localhost:7050';
 
-  config.insuranceOrg.peer.url = 'grpcs://localhost:7051';
-  config.shopOrg.peer.url = 'grpcs://localhost:8051';
-  config.repairServiceOrg.peer.url = 'grpcs://localhost:9051';
+  config.insuranceOrg.peer.url = 'grpc://localhost:7051';
+  config.shopOrg.peer.url = 'grpc://localhost:8051';
+  config.repairServiceOrg.peer.url = 'grpc://localhost:9051';
 
-  config.insuranceOrg.peer.eventHubUrl = 'grpcs://localhost:7053';
-  config.shopOrg.peer.eventHubUrl = 'grpcs://localhost:8053';
-  config.repairServiceOrg.peer.eventHubUrl = 'grpcs://localhost:9053';
+  config.insuranceOrg.peer.eventHubUrl = 'grpc://localhost:7053';
+  config.shopOrg.peer.eventHubUrl = 'grpc://localhost:8053';
+  config.repairServiceOrg.peer.eventHubUrl = 'grpc://localhost:9053';
 
   config.insuranceOrg.ca.url = 'https://localhost:7054';
   config.shopOrg.ca.url = 'https://localhost:8054';
   config.repairServiceOrg.ca.url = 'https://localhost:9054';
 }
 
-// Setup root certificates
-const basePath = resolve(__dirname, '../../certs');
-config.orderer0.pem = readFileSync(resolve(basePath, 'ordererOrg.pem')).toString();
-config.insuranceOrg.peer.pem = readFileSync(resolve(basePath, 'insuranceOrg.pem')).toString();
-config.shopOrg.peer.pem = readFileSync(resolve(basePath, 'shopOrg.pem')).toString();
-config.repairServiceOrg.peer.pem = readFileSync(resolve(basePath, 'repairServiceOrg.pem')).toString();
+// // Setup root certificates
+// const basePath = resolve(__dirname, '../../certs');
+// config.orderer0.pem = readFileSync(resolve(basePath, 'ordererOrg.pem')).toString();
+// config.insuranceOrg.peer.pem = readFileSync(resolve(basePath, 'insuranceOrg.pem')).toString();
+// config.shopOrg.peer.pem = readFileSync(resolve(basePath, 'shopOrg.pem')).toString();
+// config.repairServiceOrg.peer.pem = readFileSync(resolve(basePath, 'repairServiceOrg.pem')).toString();
 
 export default config;
