1ac99e3ff04e06426491da100606aeae4bbe7fea
Angel Milanov <milanov@de.ibm.com>
2017-06-13 08:55:56 +0200

    Upgrade to newer version of react cookie.

--- web/src/shared/getLocale.js
+++ web/src/shared/getLocale.js
@@ -1,10 +1,12 @@
-import cookie from 'react-cookie';
+import { Cookies } from 'react-cookie';
 
-const supportedLocales = ['en', 'en-US', 'de', 'de-DE', 'de-AT', 'de-CH']
+const supportedLocales = ['en', 'en-US', 'de', 'de-DE', 'de-AT', 'de-CH'];
+
+const cookies = new Cookies();
 
 export default function getLocale() {
   // Cookies have priority over browser and defaults
-  const appLangCookie = cookie.load('applang');
+  const appLangCookie = cookies.get('applang');
   const suggestedLocale = appLangCookie || (window.navigator ? window.navigator.language : 'en');
 
   if (supportedLocales.includes(suggestedLocale)) {
