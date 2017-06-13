7ca91a0b9f0b8e2a67419627372b731d232be626
Angel Milanov <milanov@de.ibm.com>
2017-06-13 10:50:58 +0200

    Properly sets up postcss.

--- web/webpack.config.prod.js
+++ web/webpack.config.prod.js
@@ -1,5 +1,6 @@
 import webpack from 'webpack';
 import { resolve } from 'path';
+import autoprefixer from 'autoprefixer';
 
 const GLOBALS = {
   'process.env.NODE_ENV': JSON.stringify('production')
@@ -33,10 +34,25 @@ export default {
   module: {
     rules: [
       { test: /\.js$/, use: ['babel-loader'] },
-      { test: /(\.css)$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /(\.css)$/, use: ['style-loader', 'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer('last 5 versions', 'ie 10')]
+            }
+          }
+        ]
+      },
       {
         test: /(\.scss)$/, use: [
-          'style-loader', 'css-loader', 'postcss-loader',
+          'style-loader', 'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer('last 5 versions', 'ie 10')]
+            }
+          },
           {
             loader: 'sass-loader',
             options: {
@@ -44,7 +60,8 @@ export default {
                 resolve(__dirname, 'node_modules/normalize-scss/sass')
               ]
             }
-          }]
+          }
+        ]
       },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: 'file-loader' },
       { test: /\.(woff|woff2)$/, use: 'url?prefix=font/&limit=5000' },
